dd5250bc4a43f60dfa6a53be675ef021abd39c92
dims140 <gamepep8881@gmail.com>
2026-05-09 22:43:51 +0800

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -5,38 +5,13 @@ export default {
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/images/splash-icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
     "android": {
-      "package": "com.anindiyasaputra.signtalker",
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/android-icon-foreground.png",
-        "backgroundColor": "#ffffff"
-      }
+      "package": "com.anindiyasaputra.signtalker"
     },
     "extra": {
       "eas": {
         "projectId": "b464565d-0850-49a7-83de-74ccb072a72c"
       }
-    },
-    "plugins": [
-      [
-        "expo-build-properties",
-        {
-          "android": {
-            "extraMavenRepos": [
-              "https://www.jitpack.io"
-            ]
-          }
-        }
-      ]
-    ]
+    }
   }
 }
