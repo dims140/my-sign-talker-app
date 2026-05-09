af840058636f2a1d4189248c26d132261f5577ec
dims140 <gamepep8881@gmail.com>
2026-05-09 22:08:47 +0800

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -5,27 +5,12 @@ export default {
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/images/splash-icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
-    },
     "android": {
+      "package": "com.anindiyasaputra.signtalker",
       "adaptiveIcon": {
         "foregroundImage": "./assets/images/android-icon-foreground.png",
         "backgroundColor": "#ffffff"
-      },
-      "package": "com.anindiyasaputra.signtalker"
-    },
-    "web": {
-      "favicon": "./assets/images/favicon.png"
+      }
     },
     "extra": {
       "eas": {
