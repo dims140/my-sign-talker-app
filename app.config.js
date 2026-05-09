6af651e761f474623438cf03a34e99dd24bbae2c
dims140 <gamepep8881@gmail.com>
2026-05-09 22:51:47 +0800

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,16 +1,29 @@
 export default {
   "expo": {
     "name": "Sign Talker",
-    "slug": "sign-talker-app-osvfnjwcfe0c4hmeej-az",
+    "slug": "sign-talker-final",
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
+    "userInterfaceStyle": "light",
+    "splash": {
+      "image": "./assets/images/splash-icon.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "assetBundlePatterns": [
+      "**/*"
+    ],
     "android": {
-      "package": "com.anindiyasaputra.signtalker"
+      "package": "com.anindiyasaputra.signtalker",
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/images/android-icon-foreground.png",
+        "backgroundColor": "#ffffff"
+      }
     },
     "extra": {
       "eas": {
-        "projectId": "b464565d-0850-49a7-83de-74ccb072a72c"
+        "projectId": "7bac762e-d8c3-457e-8e60-748e001bb532"
       }
     }
   }
